7713ea841a6fe2db5d1283f6bfc2eee6dc21b037
Vitor <you@example.com>
2024-11-26 12:21:56 -0300

    feat: Criado footer, criada página main

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,3 +1,11 @@
-export default function Footer(){
-    
+import "@styles/footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="site-footer">
+      <div className="footer-content">
+        <p>Desenvolvido por: Eduardo Bezerra e Vitor Jimenez</p>
+      </div>
+    </footer>
+  );
 }
